67a3e435bc684deb84af4dad4f221658c26d14ce
Melnikolaidis <melnikolaidis@hotmail.com>
2017-02-10 10:41:07 +0200

    Add files via upload

---
+++ company.php
@@ -0,0 +1,83 @@
+<main>
+<div class="block block-bordered-lg p-b-0 app-block-stats">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-7 col-sm-6">
+				<img
+					src="<?php echo base_url(); ?>assets/images/products.jpg"
+					class="app-translate-5"
+					data-transition="entrance">
+				<hr class="m-t-0 m-b-lg m-x-auto visible-xs">
+			</div>
+			<div class="col-md-5 col-sm-6 text-sm-left m-x-auto">
+				<p class="lead">
+					Με πάνω από 20 χρόνια εμπειρίας <br> στο χώρο του ανταλλακτικού.<br>
+					Στο κατάστημα μας θα βρείτε:
+				</p>
+					<ul>
+						<li>Ανταλλακτικά Service</li>
+						<li>Ανταλλακτικά Κινητήρα</li>
+						<li>Ηλεκτρικά Κινητήρα</li>
+						<li>Σύστημα Διεύθυνης & Ανάρτησης</li>
+						<li>Είδη Φανοποιϊας</li>
+						<li>Αξεσουάρ</li>
+					</ul>
+			</div>
+				
+		</div>
+	</div>
+</div>
+<div class="block block-bordered-lg">
+	<div class="container">
+		<div class="row">
+		<div class="col-sm-6">
+			<ul class="featured-list">
+				<li class="m-b-lg">
+					<div class="featured-list-icon text-primary">
+						<img src="<?php echo base_url(); ?>assets/images/brands/bmw-logo.png" width="32">
+					</div>
+						<p class="featured-list-icon-text m-b-0"><strong>Γνήσια ανταλλακτικα BMW και MINI</strong></p>
+						<p class="featured-list-icon-text">
+						Μεγάλη ποικιλία γνήσιων ανταλλακτικών BMW και Mini. Επίσης, δίνεται η δυνατότητα εύρεσης ανταλλακτικών
+						βάσει αναγνώρισης πλαισίου.
+						</p>
+				</li>
+				<li class="m-b-lg">
+					<div class="featured-list-icon text-primary">
+						<img src="<?php echo base_url(); ?>assets/images/brands/febi-logo.jpg" width="32">
+					</div>
+						<p class="featured-list-icon-text m-b-0"><strong>Ανταλλακτικά για ευρωπαϊκά και ασιατικά αυτοκινήτα</strong></p>
+						<p class="featured-list-icon-text">
+						Μεγάλη ποικιλία ανταλλακτικών για όλα τα ευρωπαϊκά και ασιατικά αυτοκίνητα. Ανταλλακτικά πρώτης τοποθέτησης, υψηλής ποιότητας και ανταγωνιστικής τιμής.
+						</p>
+				</li>
+			</ul>
+		</div>
+		<div class="col-sm-6">
+			<ul class="featured-list">
+				<li class="m-b-lg">
+					<div class="featured-list-icon text-primary">
+						<img src="<?php echo base_url(); ?>assets/images/brands/fiat-logo.png" width="32">
+					</div>
+						<p class="featured-list-icon-text m-b-0"><strong>Γνήσια ανταλλακτικα Fiat</strong></p>
+						<p class="featured-list-icon-text">
+						Μεγάλη ποικιλία γνήσιων ανταλλακτικών του Group Fiat. Επίσης, δίνεται η δυνατότητα εύρεσης ανταλλακτικών
+						βάσει αναγνώρισης πλαισίου.
+						</p>
+				</li>
+				<li class="m-b-lg">
+					<div class="featured-list-icon text-primary">
+						<img src="<?php echo base_url(); ?>assets/images/visa-master.png" width="52">
+					</div>
+						<p class="featured-list-icon-text m-b-0"><strong>Δυνατότητα πληρωμής με καρτα</strong></p>
+						<p class="featured-list-icon-text">
+						Δίνεται η δυνατότητα πληρωμής με χρεωστική ή πιστωτική κάρτα*. Επίσης, με αγορές άνω των &euro;300 και &euro;500 δίνεται η δυνατότητα εξόφλησης σε 3 και 5 άτοκες δόσεις αντίστοιχα.
+						</p>
+						<p class="text-muted">*Εξαιρούνται οι κάρτες τις American Express και Dyner's</p>
+				</li>
+			</ul>
+		</div>
+		</div>
+	</div>
+</div>
+</main>
